8b5ec5bf205f0a7b1b6adc7f81cbc9e8cd56d3dd
TristanAlexa <tristan.alexagoode@gmail.com>
2022-03-08 16:28:17 -0500

    Deleted unused files

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections;
+﻿/**
+    @file: GameManager.cs
+    Tracking game states and player states. Prints U.I.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using Assets;//importing namespace of generic singleton
+using Assets;
 using UnityEngine.SceneManagement;
 
 //Use to keep track of score, player/game states (rolling die, moving player)
 public class GameManager : GenericSingleton<GameManager>
 {
-   //method to restart game
+   //Restart game can be called to reload the entire game
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -58,6 +58,7 @@ public class Player : MonoBehaviour
             {
                 
                 MoveToNextTile(blueStart.transform.position);
+                
 
             }
             
